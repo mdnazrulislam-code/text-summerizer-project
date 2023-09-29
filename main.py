c068a5dd481525e8d307b0e9c1f1365f407b9e6e
Md Nazrul Islam <arfanislamabir@gmail.com>
2023-09-30 01:43:08 +0600

    model trainer pipeline added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummerizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummerizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
+
 
 from textSummerizer.logging import logger
 
@@ -31,6 +33,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
